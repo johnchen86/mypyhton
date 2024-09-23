372dd8fae02be190a80ce3a89d5022ae531133df
johnchen <58089384@qq.com>
2024-09-23 16:39:58 +0000

    增加了类的相关练习

--- mypython.py
+++ mypython.py
@@ -29,9 +29,81 @@
 #     output += emojis.get(word,word) + ' '
 # print(output)
 
-def greet_user(first_name, last_name):
-    """显示简单的问候语"""
-    print(f"Hello, {first_name.title()} {last_name.title()}!")
+# # 这是第一个函数练习
+# def greet_user(first_name, last_name):
+#     """显示简单的问候语"""
+#     print(f"Hello, {first_name.title()} {last_name.title()}!")
 
-greet_user(last_name='eson',first_name='chen')
-print("\n Finished!")
+# greet_user(last_name='eson',first_name='chen')
+# print("\n Finished!")
+
+# 这是一个函数的练习
+# def square(number):
+#     """返回一个数的平方"""
+#     return number ** 2
+# print(square(100 ))
+
+# def emoji_converter(message):
+#     if not isinstance(message, str):
+#         raise ValueError("Input must be a string")
+
+#     try:
+#         words = message.split(' ')
+#         emojis = {':)': '😊', ':(': '😢'}
+#         return ' '.join(emojis.get(word, word) for word in words)
+#     except Exception as e:
+#         print(f"Error: {e}")
+#         return None
+
+# # message = input('请输入一个短语： ')
+# # print(emoji_converter(message))
+
+# def test_emoji_converter():
+#     # 正常输入测试
+#     assert emoji_converter("Hello :) World") == "Hello 😊 World"
+    
+#     # 空字符串测试
+#     assert emoji_converter("") == ""
+    
+#     # 特殊字符测试
+#     assert emoji_converter("Hello 123 :) !@#") == "Hello 123 😊 !@#"
+    
+#     # 非字符串输入测试
+#     try:
+#         emoji_converter(123)
+#     except ValueError as e:
+#         assert str(e) == "Input must be a string"
+
+# test_emoji_converter()
+
+
+# try:
+#     age = int(input("Age: "))
+#     income = 20000
+#     risk = income / age
+#     print(age)
+# except ValueError: 
+#     print("Invalid value")
+# except ZeroDivisionError:
+#     print("Age can't be 0")
+# except
+
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y 
+ 
+# point = Point(1, 2)
+# print(f'{point.x} , {point.y}')
+# print(point.y)
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        print(f"{self.name} is {self.age} years old")
+    def talk(self):
+        print(f"{self.name} says hello!")
+
+p = Person("Eson", 30)
+p.talk()
